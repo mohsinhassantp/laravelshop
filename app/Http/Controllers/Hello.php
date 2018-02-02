09395cf89b7474b1878d7c1bc3df295cb15473c7
mastermind <mastermind>
2018-02-02 23:14:18 +0800

    initial commit

---
+++ app/Http/Controllers/Hello.php
@@ -0,0 +1,26 @@
+<?php
+
+/*namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;*/
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class Hello extends Controller
+{
+
+    public function index()
+    {
+        return 'hello world from controller : )';
+    }
+    public function show($name)
+    {
+        return view('hello',array('name' => $name));
+    }
+}

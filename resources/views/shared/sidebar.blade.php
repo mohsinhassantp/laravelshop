908863c15a9ab7dfb3d5d5b911265e740fee8c7b
mastermind <mastermind>
2018-02-03 17:04:40 +0800

    add db driven categories and brands in view

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,18 +1,19 @@
 <h2>Category</h2>
 <div class="panel-group category-products" id="accordian"><!--category-productsr-->
+    @foreach ($categories as $category)
     <div class="panel panel-default">
         <div class="panel-heading">
             <h4 class="panel-title">
                 <a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
                     <span class="badge pull-right"><i class="fa fa-plus"></i></span>
-                    Sportswear
+                    {{$category->name }}
                 </a>
             </h4>
         </div>
         <div id="sportswear" class="panel-collapse collapse">
             <div class="panel-body">
                 <ul>
-                    <li><a href="{{url('products/brands/nike/sportswear')}}">Nike </a></li>
+
                     <li><a href="{{url('products/brands/under-armour/sportswear')}}">Under Armour </a></li>
                     <li><a href="{{url('products/brands/adidas/sportswear')}}">Adidas </a></li>
                     <li><a href="{{url('products/brands/puma/sportswear')}}">Puma</a></li>
@@ -21,7 +22,8 @@
             </div>
         </div>
     </div>
-    <div class="panel panel-default">
+    @endforeach
+    {{--<div class="panel panel-default">
         <div class="panel-heading">
             <h4 class="panel-title">
                 <a data-toggle="collapse" data-parent="#accordian" href="#mens">
@@ -103,20 +105,22 @@
         <div class="panel-heading">
             <h4 class="panel-title"><a href="{{url('products/categories/shoes')}}">Shoes</a></h4>
         </div>
-    </div>
+    </div>--}}
 </div><!--/category-products-->
 
 <div class="brands_products"><!--brands_products-->
     <h2>Brands</h2>
     <div class="brands-name">
         <ul class="nav nav-pills nav-stacked">
-            <li><a href="{{url('products/brands/acne')}}"> <span class="pull-right">(50)</span>Acne</a></li>
-            <li><a href="{{url('products/brands/grune-erde')}}"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
+            @foreach($brands as  $brand)
+            <li><a href="{{url('products/brands/acne')}}"> <span class="pull-right">(50)</span>{{$brand->name}}</a></li>
+            @endforeach
+            {{--<li><a href="{{url('products/brands/grune-erde')}}"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
             <li><a href="{{url('products/brands/albiro')}}"> <span class="pull-right">(27)</span>Albiro</a></li>
             <li><a href="{{url('products/brands/ronhill')}}"> <span class="pull-right">(32)</span>Ronhill</a></li>
             <li><a href="{{url('products/brands/oddmolly')}}"> <span class="pull-right">(5)</span>Oddmolly</a></li>
             <li><a href="{{url('products/brands/boudestijn')}}"> <span class="pull-right">(9)</span>Boudestijn</a></li>
-            <li><a href="{{url('products/brands/rosch-creative-culture')}}"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+            <li><a href="{{url('products/brands/rosch-creative-culture')}}"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>--}}
         </ul>
     </div>
 </div><!--/brands_products-->
